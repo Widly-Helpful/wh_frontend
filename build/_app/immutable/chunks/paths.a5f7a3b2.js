a1fcf86e0810fdb836775f1b295f6d1aa4f7dec8
robwooner <robw@gointerject.com>
2023-09-13 16:53:19 -0700

    build?

---
+++ build/_app/immutable/chunks/paths.a5f7a3b2.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_vqj2am)==null?void 0:s.base)??"/wh_frontend";var a;const t=((a=globalThis.__sveltekit_vqj2am)==null?void 0:a.assets)??e;export{t as a,e as b};
